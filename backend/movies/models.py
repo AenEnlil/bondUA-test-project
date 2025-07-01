de8b1689d00f71f50f26104d3ddaa3d837acd1fe
AenEnlil <morphinegermanika@gmail.com>
2025-07-01 15:16:53 +0300

    updated db

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    release_date = models.DateField()
+    release_year = models.IntegerField()
     people = models.ManyToManyField('movie_persons.Person', through='movie_persons.MoviePerson', related_name='movies')
