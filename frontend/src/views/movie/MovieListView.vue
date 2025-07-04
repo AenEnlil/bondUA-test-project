5710bf29893f6ab3db6121f964927700b15e9257
AenEnlil <morphinegermanika@gmail.com>
2025-07-04 18:43:50 +0300

    moved buttons

--- frontend/src/views/movie/MovieListView.vue
+++ frontend/src/views/movie/MovieListView.vue
@@ -1,21 +1,21 @@
 <template>
     <div v-if="loading"><Loader /></div>
     <div v-else>
+        <div>
+            <svg
+                width="32px" height="32px" viewBox="0 0 24 24"
+                @click = 'showFilters = true'
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12H16M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19H20M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+        </div>
+        <div class="create-movie-button">
+                <button @click="showModalWithForm">Add movie</button>
+        </div>
         <div v-if="movies_count === 0" class='no-movies'>
             <p class="no-movies-text">Movies not found</p>
-            <div class="create-movie-button">
-                <button @click="showModalWithForm">Add movie</button>
-            </div>
         </div>
         <div v-else>
-            <div>
-                <svg
-                    width="32px" height="32px" viewBox="0 0 24 24"
-                    @click = 'showFilters = true'
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12H16M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19H20M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round"/>
-                </svg>
-            </div>
             <div class="movies-list">
                 <Movie
                     v-for='movie in movies'
@@ -30,9 +30,6 @@
                 <div class="pagination">
                     <button :disabled="!previous_page" @click="goToPreviousPage"> Back </button>
                     <button :disabled="!next_page" @click="goToNextPage"> Next</button>
-                </div>
-                <div class="create-movie-button">
-                    <button @click="showModalWithForm">Add movie</button>
                 </div>
             </div>
         </div>
